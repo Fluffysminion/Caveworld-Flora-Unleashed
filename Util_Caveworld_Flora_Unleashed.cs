53e7717f4349a2314d60fcea9a5516e91164696c
Fluffysminion <87177846+Fluffysminion@users.noreply.github.com>
2021-07-09 10:29:23 +0100

    Add files via upload

---
+++ Util_Caveworld_Flora_Unleashed.cs
@@ -0,0 +1,10 @@
+﻿using Verse;
+
+public static class Util_Caveworld_Flora_Unleashed
+{
+	public static ThingDef fungiponicsBasinDef => ThingDef.Named("ItW_FungiponicsBasin");
+	public static ThingDef MyceliumDef => ThingDef.Named("ItW_Mycelium");
+	public static ThingDef MoteGleamcapSmokeDef => ThingDef.Named("Mote_GleamcapSmoke");
+	public static HediffDef gleamcapSmokeDef => HediffDef.Named("ItW_HediffGleamcapSmoke");
+
+}
