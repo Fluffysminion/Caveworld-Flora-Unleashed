8eb0e29783c8613baafb2bd4341157c84a2c019c
Fluffysminion <87177846+Fluffysminion@users.noreply.github.com>
2021-07-16 10:30:47 +0100

    the actual code

---
+++ Source/Caveworld Flora Unleashed/Caveworld Flora Unleashed/Building_FungiponicsBasin.cs
@@ -0,0 +1,23 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Caveworld_Flora_Unleashed
+{
+	public class Building_FungiponicsBasin : Building_PlantGrower
+	{
+		public override string GetInspectString()
+		{
+			float temperature = GenTemperature.GetTemperatureForCell(base.Position, base.Map);
+			ThingDef_FruitingBody fruitingBodyDef = GetPlantDefToGrow() as ThingDef_FruitingBody;
+			if (temperature < (float)fruitingBodyDef.minGrowTemperature)
+			{
+				return "Caveworld_Flora_Unleashed.CannotGrowTooCold".Translate();
+			}
+			if (temperature > (float)fruitingBodyDef.maxGrowTemperature)
+			{
+				return "Caveworld_Flora_Unleashed.CannotGrowTooHot".Translate();
+			}
+			return "Caveworld_Flora_Unleashed.Growing".Translate();
+		}
+	}
+}
